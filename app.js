ab9c5ffd7f37a966acca9a3979995f31d94b0b36
Tom Beadie <>
2024-10-17 10:08:42 -0400

    Create refresh list function

--- app.js
+++ app.js
@@ -10,4 +10,31 @@ function getItems() {
   return JSON.parse(value);
 }
 
+function setItems(items){
+    const itemsJson = JSON.stringify(items)
+    localStorage.setItem('todo', itemsJSON)
+}
+
+function addItem(){
+    items.unshift({
+        description: '',
+        completed: false
+    })
+    setItems(items)
+    refreshList()
+}
+
+function refreshList(){
+    items_container.innerHTML  = ''
+    for (const item of items){
+        const itemElement = item.item_template.content.cloneNode(true)
+        const descriptionInput = itemElement.querySelector('.item-description')
+        const completedInput = itemElement.querySelector('.item-completed')
+
+        descriptionInput.value = item.description
+        completedInput.checked = item.completed
+
+    }
+}
+
 console.log(items);
