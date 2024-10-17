f8d43b6643f8397abead0f6dcd5d9dc4fe572c0a
Tom Beadie <>
2024-10-17 09:41:32 -0400

    Fetch items from local storage

--- app.js
+++ app.js
@@ -1,2 +1,13 @@
+const items_container = document.getElementById("items");
+const item_template = document.getElementById("itemTemplate");
+const add_btn = document.getElementById("add");
 
-console.log('working')
+let items = getItems();
+
+function getItems() {
+  const value = localStorage.getItem("todo") || "[]";
+
+  return JSON.parse(value);
+}
+
+console.log(items);
